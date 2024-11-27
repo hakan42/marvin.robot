ae6e7f7bc8d250af247a723e6e20514727c86ea2
Geir Eilertsen <eilertsen@corporater.com>
2024-11-27 14:09:53 +0100

    Added test of BrainResponder

---
+++ marvin.robot.core/src/test/java/com/assetvisor/marvin/robot/domain/brain/BrainResponderTest.java
@@ -0,0 +1,49 @@
+package com.assetvisor.marvin.robot.domain.brain;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import com.assetvisor.marvin.robot.domain.communication.ForConvertingTextToSpeech;
+import com.assetvisor.marvin.robot.domain.communication.ForMessaging;
+import com.assetvisor.marvin.robot.domain.communication.SpeechBuffer;
+import com.assetvisor.marvin.robot.domain.environment.EnvironmentDescription;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class BrainResponderTest {
+    @Mock
+    private ForMessaging forMessaging;
+    @Mock
+    private ForConvertingTextToSpeech forConvertingTextToSpeech;
+    @Spy
+    private SpeechBuffer speechBuffer;
+    @InjectMocks
+    private BrainResponder brainResponder;
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldRespond() {
+        // Given
+        String message = "message";
+        byte[] bytes = {1, 2, 3};
+
+        given(forConvertingTextToSpeech.convert(message)).willReturn(bytes);
+
+        // When
+        ArgumentCaptor<List<EnvironmentDescription>> captor = ArgumentCaptor.forClass(List.class);
+        brainResponder.respond(message);
+        // Then
+        verify(forConvertingTextToSpeech).convert(message);
+        verify(speechBuffer).add(bytes);
+        verify(forMessaging).message(message);
+        verifyNoMoreInteractions(forMessaging, forConvertingTextToSpeech, speechBuffer);
+    }
+}
