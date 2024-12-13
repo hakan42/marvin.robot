7753bb1ee1a84e84ebcc4d0ee95a467c8a1fb470
Geir Eilertsen <eilertsen@corporater.com>
2024-12-13 11:46:06 +0100

    Added CORS

--- marvin.interaction.web/src/main/java/com/assetvisor/marvin/interaction/web/controllers/HumanInteractionsController.java
+++ marvin.interaction.web/src/main/java/com/assetvisor/marvin/interaction/web/controllers/HumanInteractionsController.java
@@ -6,12 +6,14 @@ import jakarta.annotation.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class HumanInteractionsController {
 
