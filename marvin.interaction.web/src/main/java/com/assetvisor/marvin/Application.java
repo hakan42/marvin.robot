c56d29846d341021934004908194f617943f4c25
Geir Eilertsen <eilertsen@corporater.com>
2024-12-18 15:06:01 +0100

    Fixed swagger https

--- marvin.interaction.web/src/main/java/com/assetvisor/marvin/Application.java
+++ marvin.interaction.web/src/main/java/com/assetvisor/marvin/Application.java
@@ -1,10 +1,13 @@
 package com.assetvisor.marvin;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@OpenAPIDefinition(servers = @Server(url = "/", description = "Default server"))
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
