0f330d95796697ee073ee26726d1d85bdb74ce5d
Geir Eilertsen <eilertsen@corporater.com>
2024-12-18 14:06:32 +0100

    Disabled CORS

--- marvin.interaction.web/src/main/java/com/assetvisor/marvin/interaction/web/config/WebConfiguration.java
+++ marvin.interaction.web/src/main/java/com/assetvisor/marvin/interaction/web/config/WebConfiguration.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
 @Configuration
@@ -29,5 +31,15 @@ public class WebConfiguration {
     @Bean
     public AuthenticationEventPublisher authenticationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
         return new DefaultAuthenticationEventPublisher(applicationEventPublisher);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/");
+            }
+        };
     }
 }
